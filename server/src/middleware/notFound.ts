4565046f5469e1b91d355f20b475bde17d58f86c
MehtaHimanshi <mehtahimanshi456@gmail.com>
2025-10-17 14:01:22 +0530

    Delete server directory

--- server/src/middleware/notFound.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-import { ApiResponse } from '../types';
-
-export const notFound = (req: Request, res: Response<ApiResponse>) => {
-  res.status(404).json({
-    success: false,
-    message: `Route ${req.originalUrl} not found`
-  });
-};
