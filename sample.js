c48013fa7ddaca296024d37d189b0784c3dc5b4e
Mehta_Himanshi <mehtahimanshi456@gmail.com>
2025-10-17 13:10:49 +0530

    Initial commit - moved project from IRFIS-FS-Project

---
+++ sample.js
@@ -0,0 +1,50 @@
+//closure
+//event handler
+//callback function
+//asynchronous programming
+//promise
+//async await
+//fetch api
+//json
+//local storage
+//session storage
+//cookies
+//web storage api
+//dom manipulation
+//event delegation
+//template literals
+//destructuring
+//spread operator
+//rest operator
+//arrow functions
+//higher order functions
+//map
+//filter
+//reduce
+//forEach
+//set
+//map
+//weakSet
+//weakMap
+//class
+//inheritance
+//modules
+//import export
+//error handling
+//try catch
+//throw
+//custom error
+//debugging
+//console
+//breakpoints
+//network tab
+//performance tab
+//memory tab
+//application tab
+//security tab
+//accessibility tab
+//lighthouse
+//best practices
+//seo
+//pwa
+//service workers           
